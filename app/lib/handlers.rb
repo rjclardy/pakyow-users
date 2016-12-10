774557cd1d3bc9c0c32b9d71c6b481cc7cd54dfd
Ryan Clardy <ryan@metabahn.com>
2016-12-10 15:34:43 -0600

    Prepare nav after error handlers

--- app/lib/handlers.rb
+++ app/lib/handlers.rb
@@ -1,19 +1,19 @@
 Pakyow::App.routes :handlers do
   include SharedRoutes
 
-  handler 401 do
+  handler 401, after: [:prepare_nav] do
     presenter.path = "errors/401"
   end
 
-  handler 403 do
+  handler 403, after: [:prepare_nav] do
     presenter.path = "errors/403"
   end
 
-  handler 404 do
+  handler 404, after: [:prepare_nav] do
     presenter.path = "errors/404"
   end
 
-  handler 500 do
+  handler 500, after: [:prepare_nav] do
     presenter.path = "errors/500"
   end
 end
